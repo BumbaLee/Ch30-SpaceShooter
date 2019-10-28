fbf7ba48b472eece8f7754a0c6d7ce954772fb11
Christopher Wind <christopherwind843@gmail.com>
2019-10-28 18:35:19 -0400

    uhhhhhh wtf is al this

---
+++ Assets/Scripts/Enemy_2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_2 : Enemy
+{
+    [Header("Set in Inspector: Enemy_2")]
+    //determines how much the sine wave wil affect the movement
+    public float sinEccentricity= .6f;
+    public float lifeTime=10;
+    
+}
