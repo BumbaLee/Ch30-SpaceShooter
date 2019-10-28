940d8643307af7b76a4942c9a5069cf28e86d3bb
Christopher Wind <christopherwind843@gmail.com>
2019-10-28 14:19:11 -0400

    shield script, bounds check script

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("Set in Inspector")]
+    public float rotationsPerSecond=0.1f;
+
+    [Header ("Set Dynamically")]
+    public int levelShown= 0; 
+
+    //This non public variable will not appear in the Inspector
+
+    Material mat;
+
+    void Start(){
+        mat=GetComponent<Renderer>().material;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        //read the current shield level from the Hero Singleton
+        int currLevel=Mathf.FloorToInt(Hero.S.shieldLevel);
+
+        //if this is different from levelShown
+        if(levelShown!=currLevel){
+            levelShown=currLevel;
+            //adjust the texture offset to show different shield level
+            mat.mainTextureOffset= new Vector2(0.2f*levelShown,0);
+        }
+        //rotate the shield a bit every frame in a time-based way
+        float rZ= -(rotationsPerSecond*Time.time*360)%360f;
+        transform.rotation=Quaternion.Euler(0,0,rZ);
     }
+
 }
